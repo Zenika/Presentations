1f7eba590c7a03df346f2164c47bad8a1cb13462
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2015-10-27 15:47:17 +0100

    clean up Android prez

--- 20111213_NantesGTUG_Android-pour-les-newbees/ZeniPark/src/com/zenika/android/park/app/Conf.java
+++
@@ -1,6 +0,0 @@
-package com.zenika.android.park.app;
-
-public class Conf {
-
-	public static final boolean	DEBUG	= true;
-}
--- 20111213_NantesGTUG_Android-pour-les-newbees/ZeniPark/src/com/zenika/android/park/app/Intents.java
+++
@@ -1,6 +0,0 @@
-package com.zenika.android.park.app;
-
-public class Intents {
-
-	public static final String	ACTION_START_UPDATE_SERVICE	= "com.zenika.android.park.updates.ACTION_START_SERVICE";
-}
